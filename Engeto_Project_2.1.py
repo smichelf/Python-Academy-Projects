5b451a1d89259d7f4528858a1ba7611d4f0391ea
smichelf <57625711+smichelf@users.noreply.github.com>
2021-09-25 14:10:34 +0200

    Add files via upload

--- Engeto_Project_2.1.py
+++ Engeto_Project_2.1.py
@@ -1,49 +1,59 @@
-import random
-oddelovac='-' * 48
-konec = False
-print('Hi there!\n' + oddelovac)
-while not konec:
-    print("I've generated a random 4 digit number for you.\n" + "Let's play a bulls and cows game.\n" + oddelovac)
+
+def welcome_gamer():
+    print('Hi there!\n' + oddelovac)
 #
 # computer number generation
 #
-    moznosti=['1','2','3','4','5','6','7','8','9']
-    pcislo=list()
+def generate_number():
+    moznosti = list('123456789')
+    pcc = list()
     for i in range(4):
-        pcislo.append(random.choice(moznosti))
-        moznosti=['0','1','2','3','4','5','6','7','8','9']
-        for c in pcislo:
+        pcc.append(random.choice(moznosti))
+        moznosti = list('0123456789')
+        for c in pcc:
             moznosti.remove(c)
+    print("I've generated a random 4 digit number for you.\n" + "Let's play a bulls and cows game.\n" + oddelovac)
+    return pcc
 #
-    bulls = 0
+# Count bulls and cows
+#
+def count_result(unumber, pnumber):
+    res = {'bulls': 0, 'cows': 0}
+    for i in range(4):
+        if unumber[i] in pnumber:
+            if pnumber.index(unumber[i]) == i:
+                res['bulls'] += 1
+            else:
+                res['cows'] += 1
+    return res
+
+def user_guessing(pnum):
+    result = {'bulls': 0, 'cows': 0}
+    global konec
     steps = 0
-#    print(pcislo)
-    while bulls < 4:
-        bulls = 0
-        cows = 0
-        obulls = 'bulls'
-        ocows = 'cows'
+    while result['bulls'] < 4:
         uscislo = input('Enter a number (0 to finish): ')
         if uscislo[0] == '0':
             konec = True
             break
-        if len(uscislo) != 4 or not uscislo.isnumeric():
-            print('Input is not numeric or not 4 digits long.')
+        if len(set(uscislo)) != 4 or not uscislo.isnumeric():
+            print('Input is not numeric, contain duplicity or is not 4 digits long.')
             continue
         ucislo=list(uscislo)
         steps += 1
-#
-# count bulls and cows
-#
-        for i in range(4):
-            if ucislo[i] in pcislo:
-                if pcislo.index(ucislo[i]) == i:
-                    bulls += 1
-                else:
-                    cows += 1
-#
-        if bulls == 1: obulls = 'bull'
-        if cows == 1: ocows = 'cow'
-        print(bulls, obulls + ',', cows,ocows, '\n' + oddelovac)
-    if not konec:
+        result=count_result(ucislo, pnum)
+        print(result['bulls'], 'bulls' if result['bulls'] != 1 else 'bull', ',',
+              result['cows'], 'cows' if  result['cows'] != 1 else 'cow', '\n' + oddelovac + '\n', end='')
+    else:
         print("Correct, you've guessed the right number\nin " + str(steps) + ' guesses!\n' + oddelovac)
+
+import random
+oddelovac='-' * 48
+konec = False
+#
+welcome_gamer()
+while not konec:
+    pcislo = generate_number()
+    user_guessing(pcislo)
+else:
+    print('Good bye player, see you soon again.')
